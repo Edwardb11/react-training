be2dc02e9a6879b771f650b18a0b24f0ee78e084
Edward Brito Diaz <37183622+Edwardb11@users.noreply.github.com>
2022-05-20 11:54:27 -0400

    fetch api for change img

--- Image-Gallery/src/components/Card.jsx
+++ Image-Gallery/src/components/Card.jsx
@@ -1,15 +1,16 @@
 import React from "react";
+import PropTypes from "prop-types";
 
-const Card = () => {
+const Card = ({ img }) => {
   return (
     <div className="card" style={{ width: "15rem" }}>
-      <img
-        src="https://images.unsplash.com/photo-1651666445280-426bae060557?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwzMzA0MTd8MHwxfHJhbmRvbXx8fHx8fHx8fDE2NTMwNTg2Nzk&ixlib=rb-1.2.1&q=80&w=200"
-        className="card-img-top"
-        alt="img"
-      />
+      <img src={img} className="card-img-top" alt="img" />
     </div>
   );
+};
+
+Card.propTypes = {
+  img: PropTypes.string,
 };
 
 export default Card;
--- Image-Gallery/src/components/Cards.jsx
+++ Image-Gallery/src/components/Cards.jsx
@@ -1,24 +1,30 @@
-import React, { useState, useEffect } from 'react'
-import Card from './Card'
+import React, { useState, useEffect } from "react";
+import Card from "./Card";
 
 const Cards = () => {
-const [images, setImages] = useState({})
+  const [images, setImages] = useState({
+    urls: {
+      regular: "",
+    },
+  });
 
-const peticion = async () => {
-const res= await fetch('https://api.unsplash.com/photos/random/?client_id=TlD4jwNraHpTsPCwcSYEHyYJNNf2pSnBnq3mgDEjmkQ')
-const data = await res.json()
-// console.log(data)
-setImages(data.urls.regular)
-}
-useEffect(() => {
+  const peticion = async () => {
+    const res = await fetch(
+      "https://api.unsplash.com/photos/random/?client_id=TlD4jwNraHpTsPCwcSYEHyYJNNf2pSnBnq3mgDEjmkQ"
+    );
+    const data = await res.json();
+    // console.log(data);
+    setImages(data);
+  };
+  useEffect(() => {
     peticion();
-},[])
+  });
 
   return (
-   <>
-   <Card/>
-   </>
-  )
-}
+    <>
+      <Card img={images.urls.regular} />
+    </>
+  );
+};
 
-export default Cards
+export default Cards;
