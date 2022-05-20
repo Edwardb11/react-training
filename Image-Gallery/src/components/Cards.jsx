74b44279143bdb51a58129e88791ade1f2d3b2e0
Edward Brito Diaz <37183622+Edwardb11@users.noreply.github.com>
2022-05-20 19:41:52 -0400

    form and update map

--- Image-Gallery/src/components/Cards.jsx
+++ Image-Gallery/src/components/Cards.jsx
@@ -2,15 +2,12 @@ import React, { useState, useEffect } from "react";
 import Card from "./Card";
 
 const Cards = () => {
-  const [images, setImages] = useState({
-    urls: {
-      regular: "",
-    },
-  });
+  const [images, setImages] = useState([]);
+  const [input, setInput] = useState('');
 
   const peticion = async () => {
     const res = await fetch(
-      "https://api.unsplash.com/photos/random/?client_id=TlD4jwNraHpTsPCwcSYEHyYJNNf2pSnBnq3mgDEjmkQ"
+      "https://api.unsplash.com/photos/?client_id=TlD4jwNraHpTsPCwcSYEHyYJNNf2pSnBnq3mgDEjmkQ"
     );
     const data = await res.json();
     // console.log(data);
@@ -19,10 +16,21 @@ const Cards = () => {
   useEffect(() => {
     peticion();
   });
+const handleSubmit =(e)=>{
+  e.preventDefault()
+const text =e.target[0].value;
+setInput(text)
+}
 
   return (
     <>
-      <Card img={images.urls.regular} />
+    <form onSubmit={handleSubmit}>
+      <label htmlFor="busqueda"> Buscar <input type='text'  name='busqueda'/></label>
+    </form>
+    <hr/>
+      {images.map((img) => {
+        return <Card key={img.id} img={img.urls.regular} />;
+      })}
     </>
   );
 };
