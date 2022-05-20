c332501cc27c4959b21424ac427106a7dc909c6f
Edward Brito Diaz <37183622+Edwardb11@users.noreply.github.com>
2022-05-20 10:59:39 -0400

    container

--- Image-Gallery/src/App.jsx
+++ Image-Gallery/src/App.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
+import Container from './components/Container';
 
 const App = () => {
   return (
     <div>
       <h1>Hello World</h1>
+      <Container />
     </div>
   )
 }
