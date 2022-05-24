dc3fc7f921b7c98938c792a50065573d14759da8
Edward Brito Diaz <37183622+Edwardb11@users.noreply.github.com>
2022-05-24 13:58:12 -0400

    creating context

--- context-api/src/App.js
+++ context-api/src/App.js
@@ -1,12 +1,15 @@
-import React from 'react'
-import AppRouter from './routes/AppRouter'
+import React, { useState } from "react";
+import { UserContext } from "./contexs/UserContext";
+import AppRouter from "./routes/AppRouter";
 
 const App = () => {
+  const [user, setUser] = useState(null);
+  // const state = [user, setUser];
   return (
-    <div>
-<AppRouter/>
-    </div>
-  )
-}
+    <UserContext.Provider value={{user,setUser}}>
+      <AppRouter />
+    </UserContext.Provider>
+  );
+};
 
-export default App
+export default App;
