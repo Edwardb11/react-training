8cc2cfac35852df41dc0c033ff5ca8c0444c6c81
Edward Brito Diaz <37183622+Edwardb11@users.noreply.github.com>
2022-05-24 11:39:18 -0400

    feat navbar

--- context-api/src/components/Header.jsx
+++ context-api/src/components/Header.jsx
@@ -1,9 +1,39 @@
-import React from 'react'
+import React from "react";
 
 const Header = () => {
   return (
-    <div className='text text-center'>Header</div>
-  )
-}
+    <nav className="navbar navbar-expand-lg navbar-light  bg-light">
+      <div className="container-fluid">
+        <h2 className="navbar-brand">Context App</h2>
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarSupportedContent"
+          aria-controls="navbarSupportedContent"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
 
-export default Header
+        <div className="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul className="navbar-nav  me-auto mb-2 mb-lg-0">
+            <li className="navbar-item">
+              <span to="/" className="nav-link active">
+                Home
+              </span>
+            </li>
+            <li className="navbar-item">
+              <span to="/about" className="nav-link">
+                About
+              </span>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default Header;
