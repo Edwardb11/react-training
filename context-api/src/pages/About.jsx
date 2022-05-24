d4c4b295c8ce51f73c91c213316b1f987de1c6ba
Edward Brito Diaz <37183622+Edwardb11@users.noreply.github.com>
2022-05-24 14:28:26 -0400

    use context for info for all app

--- context-api/src/pages/About.jsx
+++ context-api/src/pages/About.jsx
@@ -1,9 +1,30 @@
-import React from 'react'
+import React, { useContext } from "react";
+import { UserContext } from "../contexs/UserContext";
 
 const About = () => {
-  return (
-    <div>About</div>
-  )
-}
+  const { user } = useContext(UserContext);
 
-export default About
+  return (
+    <div className="container text-center mt-5">
+      <h1>About Screen</h1>
+      {!user ? (
+        <h2 className="text-danger">Please Login</h2>
+      ) : (
+        <div>
+          <h3>
+            Name: {user.data.first_name} {user.data.last_name}
+          </h3>
+          <h3>Email: {user.data.email}</h3>
+          <img
+            src={user.data.avatar}
+            alt={user.data.id}
+            width="150px"
+            height="150px"
+          />
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default About;
--- context-api/src/pages/Home.jsx
+++ context-api/src/pages/Home.jsx
@@ -1,9 +1,39 @@
-import React from 'react'
+import React, { useContext } from "react";
+import { UserContext } from "../contexs/UserContext";
+import { userData } from "../helpers/userData";
 
 const Home = () => {
-  return (
-    <div>Home</div>
-  )
-}
+  const { user, setUser } = useContext(UserContext);
 
-export default Home
+  const handleLogin = () => {
+    setUser(userData);
+  };
+
+  const handleLogout = () => {
+    setUser(null);
+  };
+
+  return (
+    <div className="container text-center mt-5">
+      <h1>Home Screen</h1>
+
+      {!user ? (
+        <button className="btn btn-primary" onClick={handleLogin}>
+          Login
+        </button>
+      ) : (
+        <>
+          <h3>
+            {" "}
+            {user.data.first_name} {user.data.last_name}
+          </h3>
+          <button className="btn btn-danger" onClick={handleLogout}>
+            Logout
+          </button>
+        </>
+      )}
+    </div>
+  );
+};
+
+export default Home;
