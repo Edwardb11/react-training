86607b21765db508256740fabc5c02ebd19bf38c
Edward Brito Diaz <37183622+Edwardb11@users.noreply.github.com>
2022-05-31 09:53:29 -0400

    layout serach and bug in womans

--- db-app-react/src/pages/SearchScreen.jsx
+++ db-app-react/src/pages/SearchScreen.jsx
@@ -1,9 +1,30 @@
-import React from 'react'
+import React from "react";
 
 const SearchScreen = () => {
   return (
-    <div>SearchScreen</div>
-  )
-}
+    <div className="container">
+      <div className="row">
+        <div className="col-6">
+          <h2>Search</h2>
+          <form>
+            <label className="form-label w-100">
+              Character
+              <input
+                placeholder="Name Character"
+                type="text"
+                className="form-control"
+                autoComplete="off"
+                value={""}
+              />
+            </label>
+            <button type="submit" className="btn btn-success w-100">
+              Search
+            </button>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default SearchScreen
+export default SearchScreen;
