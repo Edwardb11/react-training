d1d8e2d46be867b8df0168fb5e6b5224602b4b10
Edward Brito Diaz <37183622+Edwardb11@users.noreply.github.com>
2022-06-02 13:21:45 -0400

    dynamic form and bug

--- contact-book/src/components/Contacts.jsx
+++ contact-book/src/components/Contacts.jsx
@@ -1,11 +1,12 @@
 import React, { useEffect, useReducer } from "react";
+import { useState } from "react";
 import { ContactReducer } from "../reducers/ContactReducer";
 import FormsAdd from "./FormsAdd";
 import TableContacts from "./TableContacts";
 
 const init = () => {
   const contacts = localStorage.getItem("contacts");
-  return contacts ?  JSON.parse('contacts') : [];
+  return contacts ? JSON.parse(contacts) : [];
 };
 const Contacts = () => {
   const [state, dispatch] = useReducer(ContactReducer, [], init);
@@ -14,9 +15,17 @@ const Contacts = () => {
     localStorage.setItem("contacts", JSON.stringify(state));
   }, [state]);
 
+  const [formView, setFormView] = useState(false);
+
   return (
     <div className="container mt-3">
-      <FormsAdd dispatch={dispatch} />
+      <button
+        className="btn btn-success"
+        onClick={() => setFormView(!formView)}
+      >
+        {formView ? "+ Add contacts" : "- Close form"}
+      </button>
+      {!formView && <FormsAdd dispatch={dispatch} />}
       <TableContacts contacts={state} dispatch={dispatch} />
     </div>
   );
