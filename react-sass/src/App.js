37465122ed3516252442631c046dd4b69c3ce807
Edward Brito Diaz <37183622+Edwardb11@users.noreply.github.com>
2022-06-03 14:08:35 -0400

    nesting elements

--- react-sass/src/App.js
+++ react-sass/src/App.js
@@ -4,6 +4,23 @@ const App = () => {
   return (
     <div>
       <h1 className='title'>Hello World</h1>
+      <hr/>
+
+      <ul>
+        <li>
+          <a href='https://reactjs.org/'>React</a>
+        </li>
+        <li>
+          <a href='https://nextjs.org/'>Next.js</a>
+        </li>
+        <li>
+          <a href='https://sass-lang.com/'>Sass</a>
+        </li>
+        <li>
+          <a href='https://tailwindcss.com/'>Tailwind CSS</a>
+        </li>
+
+      </ul>
     </div>
   )
 }
