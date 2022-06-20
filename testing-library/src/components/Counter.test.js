562a5045f0a039e99ad1edcafdc19d563da9b548
Edward Brito Diaz <37183622+Edwardb11@users.noreply.github.com>
2022-06-20 14:29:33 -0400

    feat: replace test for it

--- testing-library/src/components/Counter.test.js
+++ testing-library/src/components/Counter.test.js
@@ -2,14 +2,14 @@ import { render } from "@testing-library/react";
 import Counter from "./Counter";
 
 describe("<Counter/>", () => {
-  test("Pruebas de que el titulo y estado se renderizan correctamente", () => {
+  it("Pruebas de que el titulo y estado se renderizan correctamente", () => {
     const wrapper = render(<Counter />);
 
     //expect es valor actual y TOBE es el valor que espera
     expect(wrapper.getByText("Counter: 0").tagName).toBe("H2");
   });
 
-  test("Prueba que 1 + 1 es 2 no es 3", () => {
+  it("Prueba que 1 + 1 es 2 no es 3", () => {
     expect(1 + 1).toBe(2);
     expect(1 + 1).not.toBe(3);
   });
