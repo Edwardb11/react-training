5f909a1b0e9147f0a423b7a50230458c4b049ab0
Edward Brito Diaz <37183622+Edwardb11@users.noreply.github.com>
2022-06-20 14:28:43 -0400

    feat: describe

--- testing-library/src/components/Counter.test.js
+++ testing-library/src/components/Counter.test.js
@@ -1,9 +1,16 @@
 import { render } from "@testing-library/react";
 import Counter from "./Counter";
 
-test("Pruebas al componente Counter", () => {
-  const wrapper = render(<Counter />);
+describe("<Counter/>", () => {
+  test("Pruebas de que el titulo y estado se renderizan correctamente", () => {
+    const wrapper = render(<Counter />);
 
-//expect es valor actual y TOBE es el valor que espera
-  expect(wrapper.getByText("Counter: 0").tagName).toBe("H2")
+    //expect es valor actual y TOBE es el valor que espera
+    expect(wrapper.getByText("Counter: 0").tagName).toBe("H2");
+  });
+
+  test("Prueba que 1 + 1 es 2 no es 3", () => {
+    expect(1 + 1).toBe(2);
+    expect(1 + 1).not.toBe(3);
+  });
 });
