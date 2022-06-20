90c3764f57e83de7ef6e71a3d0ed9a22ca1becb3
Edward Brito Diaz <37183622+Edwardb11@users.noreply.github.com>
2022-06-20 14:20:11 -0400

    feat: expect ToBe

--- testing-library/src/components/Counter.test.js
+++ testing-library/src/components/Counter.test.js
@@ -1,7 +1,9 @@
-import { render } from "@testing-library/react"
-import Counter from "./Counter"
+import { render } from "@testing-library/react";
+import Counter from "./Counter";
 
-test("Pruebas al componente Counter",()=>{
-    const wrapper = render(<Counter/>);
-    console.log(wrapper.getByText("Counter: 0").tagName)
-})
+test("Pruebas al componente Counter", () => {
+  const wrapper = render(<Counter />);
+
+//expect es valor actual y TOBE es el valor que espera
+  expect(wrapper.getByText("Counter: 0").tagName).toBe("H2")
+});
