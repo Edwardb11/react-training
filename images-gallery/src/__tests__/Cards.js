6a8fe08dc9b8003b6d1ea01f543c12aa01892a89
Edward Brito Diaz <37183622+Edwardb11@users.noreply.github.com>
2022-06-28 14:00:02 -0400

    fix

--- images-gallery/src/__tests__/Cards.js
+++ images-gallery/src/__tests__/Cards.js
@@ -1,17 +1,22 @@
 import { render, screen } from "@testing-library/react";
+import axios from "axios";
 import Cards from "../components/Cards";
+import { images } from "../__mocks__/images";
 
 describe("<Cards/>", () => {
   it("Snapshot cards", () => {
     render(<Cards />);
     expect(screen.getByLabelText("imagenes")).toMatchSnapshot();
   });
-  it("Snapshot card",async () => {
+  it("Snapshot card", async () => {
+    // Simulacion de peticion MOCK
+    axios.get = jest.fn().mockResolvedValue({ data: images });
     render(<Cards />);
+
     // console.log(await screen.findAllByLabelText("img"))
     const res = await screen.findAllByLabelText("img");
 
     // expect(res).toMatchSnapshot();
-    expect(res).toHaveLength(10)
+    expect(res).toHaveLength(10);
   });
 });
