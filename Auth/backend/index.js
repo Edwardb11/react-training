d7b6f7301dd4c28a131d900bcaaee7818b6796d7
Edward Brito Diaz <37183622+Edwardb11@users.noreply.github.com>
2022-12-15 13:40:35 -0400

    backend:Setting models, controllers and router

--- Auth/backend/index.js
+++ Auth/backend/index.js
@@ -1,13 +1,18 @@
 import express from "express";
 import db from "./config/Database.js";
+import Users from "./models/UserModel.js";
+import router from "./routes/index.js";
 
 const app = express();
 
 try {
   await db.authenticate();
   console.log("Conected");
+  //   await Users.sync();
 } catch (error) {
   console.error(error);
 }
-app.listen(5000, () => console.log("Server running"));
 
+app.use(express.json);
+app.use(router);
+app.listen(5000, () => console.log("Server running"));
