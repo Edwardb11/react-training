e5e98717e536f043f790485294b5d2ce843ea4d3
Edward Brito Diaz <37183622+Edwardb11@users.noreply.github.com>
2022-12-15 14:03:56 -0400

    Backend: fix

--- Auth/backend/index.js
+++ Auth/backend/index.js
@@ -4,15 +4,15 @@ import Users from "./models/UserModel.js";
 import router from "./routes/index.js";
 
 const app = express();
-
+const PORT = 5000;
 try {
   await db.authenticate();
   console.log("Conected");
-  //   await Users.sync();
+  await Users.sync();
 } catch (error) {
   console.error(error);
 }
 
-app.use(express.json);
+app.use(express.json());
 app.use(router);
-app.listen(5000, () => console.log("Server running"));
+app.listen(PORT, () => console.log(`Server running ${PORT}`));
