81e00f87e2e05664bb52f4500d0703db0884e2ea
Edward Brito Diaz <37183622+Edwardb11@users.noreply.github.com>
2022-12-16 12:58:54 -0400

    Frontend:Login

---
+++ Auth/frontend/src/components/Login.js
@@ -0,0 +1,41 @@
+import React from "react";
+
+const Login = () => {
+  return (
+    <section className="hero has-background-grey-light  is-fullheight is-fullwidth">
+      <div className="hero-body">
+        <div className="container">
+        <div className="columns is-centered">
+          <div className="column is-4-desktop">
+            <form className="box">
+              <div className="field mt-5">
+                <label className="label">Email o username</label>
+                <div className="controls">
+                  <input
+                    type="text"
+                    className="input"
+                    placeholder="Username"></input>
+                </div>
+              </div>
+              <div className="field mt-5">
+                <label className="label">Password</label>
+                <div className="controls">
+                  <input
+                    type="password"
+                    className="input"
+                    placeholder="****"></input>
+                </div>
+              </div>
+              <div className="field mt-5">
+                <button className="button is-success is-fullwith">Login</button>
+              </div>
+            </form>
+          </div>
+        </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Login;
