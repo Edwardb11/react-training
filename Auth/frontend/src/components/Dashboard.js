fc30107a8b90356c822d2197f259749347f29bdf
Edward Brito Diaz <37183622+Edwardb11@users.noreply.github.com>
2022-12-16 14:30:35 -0400

    Fronted:Decode

--- Auth/frontend/src/components/Dashboard.js
+++ Auth/frontend/src/components/Dashboard.js
@@ -1,10 +1,28 @@
-import React from "react";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
 import Navbar from "./Navbar";
+import jwt_decode from "jwt-decode";
+import { useNavigate } from "react-router-dom";
 
 const Dashboard = () => {
+  const [name, setName] = useState("");
+  const [token, setToken] = useState("");
+  useEffect(() => {
+    refreshToken();
+  }, []);
+
+  const refreshToken = async () => {
+    try {
+      const response = await axios.get("http://localhost:5000/token");
+      setToken(response.data.accessToken);
+      console.log(response.data.accessToken);
+      const decoded = jwt_decode(response.data.accessToken);
+        console.log(decoded);
+    } catch (error) {}
+  };
   return (
-      <>
-        <Navbar/>
+    <>
+      <Navbar />
       <h1 className="container mt-5"> welcome back: </h1>
     </>
   );
