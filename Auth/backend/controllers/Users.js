65be53253bd3c8dae58536800a027f9cdb055325
Edward Brito Diaz <37183622+Edwardb11@users.noreply.github.com>
2022-12-16 10:48:43 -0400

    backend:verify token

--- Auth/backend/controllers/Users.js
+++ Auth/backend/controllers/Users.js
@@ -4,7 +4,9 @@ import jwt from "jsonwebtoken";
 
 export const getUsers = async (req, res) => {
   try {
-    const users = await Users.findAll();
+    const users = await Users.findAll({
+      attributes: ["id", "name", "email"],
+    });
     res.json(users);
   } catch (error) {
     console.log(error);
@@ -52,7 +54,7 @@ export const Login = async (req, res) => {
       { userId, name, emails },
       process.env.ACCESS_TOKEN_SECRET,
       {
-        expiresIn: "20s",
+        expiresIn: "1h",
       }
     );
     const refreshToken = jwt.sign(
