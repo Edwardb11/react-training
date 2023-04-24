39d7b5f74bd9dff0adb2d8ede77d4b4a9f1fee50
Edwardb11 <edwardbrito11@hotmail.com>
2023-04-24 15:36:17 -0400

    Challenge structured

--- react-shop/src/App.jsx
+++ react-shop/src/App.jsx
@@ -1,10 +1,16 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Layout from "./containers/Layout";
-import Login from "./containers/Login";
 import RecoveryPassword from "./containers/RecoveryPassword";
 import NotFound from "./pages/NotFound";
 import Home from "./pages/Home";
+import Login from "./pages/Login";
+import SendEmail from "./pages/SendEmail";
+import NewPassword from "./pages/NewPassword";
+import MyAccount from "./pages/MyAccount";
+import CreateAccount from "./pages/CreateAccount";
+import Layout from "./containers/Layout";
+import Checkout from "./pages/Checkout";
+import Orders from "./pages/Orders";
 
 function App() {
   return (
@@ -18,6 +24,12 @@ function App() {
             path="/recovery-password"
             element={<RecoveryPassword />}
           />
+          <Route exact path="/send-email" element={SendEmail} />
+          <Route exact path="/new-password" element={NewPassword} />
+          <Route exact path="/account" element={MyAccount} />
+          <Route exact path="/signup" element={CreateAccount} />
+          <Route exact path="/checkout" element={Checkout} />
+          <Route exact path="/orders" element={Orders} />
           <Route path="*" element={<NotFound />} />
         </Routes>
       </Layout>
--- react-shop/src/pages/Home.jsx
+++ react-shop/src/pages/Home.jsx
@@ -1,12 +1,11 @@
 import React from "react";
-import Header from "../components/Header";
+import ProductList from "../containers/ProductList";
 
 const Home = () => {
   return (
-    <div>
-      <Header />
-      <h1>React Shop</h1>
-    </div>
+    <>
+      <ProductList />
+    </>
   );
 };
 
