d066a6caa193c7b34cdb38da8475db3d49ac4452
Edwardb11 <edwardbrito11@hotmail.com>
2023-04-24 14:55:09 -0400

    feat: noFound and home

--- react-shop/src/App.jsx
+++ react-shop/src/App.jsx
@@ -3,19 +3,22 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Layout from "./containers/Layout";
 import Login from "./containers/Login";
 import RecoveryPassword from "./containers/RecoveryPassword";
-
-function Home() {
-  return <h1>Home Page</h1>;
-}
+import NotFound from "./pages/NotFound";
+import Home from "./pages/Home";
 
 function App() {
   return (
     <BrowserRouter>
       <Layout>
         <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/Login" element={<Login />} />
-          <Route path="/recovery-password" element={<RecoveryPassword />} />
+          <Route exact path="/" element={<Home />} />
+          <Route exact path="/Login" element={<Login />} />
+          <Route
+            exact
+            path="/recovery-password"
+            element={<RecoveryPassword />}
+          />
+          <Route path="*" element={<NotFound />} />
         </Routes>
       </Layout>
     </BrowserRouter>
