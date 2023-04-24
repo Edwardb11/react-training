6d5e2e6dde6455ee4f9efc4ca72d2613f4979c71
Edwardb11 <edwardbrito11@hotmail.com>
2023-04-24 13:58:06 -0400

    feat: seeting vite and sass

---
+++ react-shop/src/App.jsx
@@ -0,0 +1,11 @@
+import "./styles/index.scss";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello, world!</h1>
+    </div>
+  );
+}
+
+export default App;
---
+++ react-shop/vite.config.js
@@ -0,0 +1,14 @@
+import { defineConfig } from "vite";
+import reactRefresh from "@vitejs/plugin-react-refresh";
+import sass from "sass";
+
+export default defineConfig({
+  plugins: [reactRefresh()],
+  css: {
+    preprocessorOptions: {
+      sass: {
+        implementation: sass,
+      },
+    },
+  },
+});
